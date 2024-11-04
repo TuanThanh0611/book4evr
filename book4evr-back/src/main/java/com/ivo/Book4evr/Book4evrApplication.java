04f49b70d5e47968ce5080b563fea3b96e7745fe
Tuanthanh0611 <nguyenhatuanthanh.110632@gmail.com>
2024-11-04 22:29:27 +0700

    ini

---
+++ book4evr-back/src/main/java/com/ivo/Book4evr/Book4evrApplication.java
@@ -0,0 +1,13 @@
+package com.ivo.Book4evr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Book4evrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Book4evrApplication.class, args);
+	}
+
+}
---
+++ book4evr-back/src/test/java/com/ivo/Book4evr/Book4evrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivo.Book4evr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Book4evrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
